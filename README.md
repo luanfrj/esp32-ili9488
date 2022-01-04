cb04e1c6cce1a3d32e7990c5e13f82b38664f6fb
Luan Ferreira Reis de Jesus <luan@luan.heliohost.org>
2022-01-03 21:15:31 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# esp32-ili9488
+Integração entre esp32 e lcd
---
+++ main/main.c
@@ -0,0 +1,43 @@
+/* WiFi station Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "ili9488.h"
+
+void app_main()
+{
+  setup_lcd_pins();
+  delay_ms(100);
+
+  printf("Iniciando LCD\n");
+  
+  init_lcd();
+
+  gpio_set_level(LCD_CS, 0);
+
+  printf("Escrevendo pixel\n");
+  write_pixel(100, 100, 255, 0, 0);
+
+  write_string("Hello world!", 0, 20, 255, 0, 0);
+  write_string("Hello world!", 10, 34, 255, 255, 255);
+  write_string("Hello world!", 20, 48, 0, 255, 0);
+  write_string("Hello world!", 30, 62, 0, 0, 255);
+
+
+  write_string("LUAN", 0, 180, 199, 67, 209);
+
+  // while(1) {
+  //   printf("Aguardando...\n");
+  //   delay_ms(2000);
+  // }
+}
